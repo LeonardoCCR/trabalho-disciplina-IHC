60b17b9541210284095ee5a318d8f06b5a3aeace
LeonardoCCR <leonardocesar01.12@gmail.com>
2025-02-15 17:13:13 -0300

    main limpa

---
+++ src/tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    theme: {
+        extend: {
+            colors: {
+                primary: "#007AFF", // Azul - Segurança e confiabilidade
+                secondary: "#34C759", // Verde - Sucesso, confirmação
+                error: "#FF3B30", // Vermelho - Atenção, erro
+                neutral: {
+                    DEFAULT: "#F2F2F7", // Cinza Claro - Fundo neutro
+                    white: "#FFFFFF", // Branco - Fundo neutro
+                },
+                text: {
+                    DEFAULT: "#1C1C1E", // Preto 90% - Contraste para textos
+                },
+            },
+        },
+    },
+    plugins: [],
+};
