f3f14b8eca9eab21cc32b2bd280b81e3608c697f
Bernardo Duarte <bernardoduarte.mg@gmail.com>
2025-02-15 18:29:36 -0300

    fazendo a página de login

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    theme: {
+        extend: {
+            colors: {
+                primary: "#007AFF", // Azul - Segurança e confiabilidade
+                secondary: "#34C759", // Verde - Sucesso, confirmação
+                error: "#FF3B30", // Vermelho - Atenção, erro
+                default: "#F2F2F7", // Cinza Claro - Fundo neutro
+                white: "#FFFFFF",
+                text: "#1C1C1E", // Preto 90% - Contraste para textos
+            },
+        },
+    },
+    plugins: [],
+};
