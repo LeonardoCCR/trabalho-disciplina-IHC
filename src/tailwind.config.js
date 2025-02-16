d232bc6b5214b0e6fdb758a2ff28ccef1c619bb2
LeonardoCCR <leonardocesar01.12@gmail.com>
2025-02-16 16:32:21 -0300

    pagina de login pronta

---
+++ src/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    theme: {
+      extend: {
+        fontFamily: {
+          sans: ['Poppins', 'sans-serif'], // Definindo 'Poppins' como padrão
+        },
+      },
+    },
+    plugins: [],
+  }
